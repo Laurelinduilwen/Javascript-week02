caf0c7dc678531a23e63bc187d19cd35dc9343b6
Laurelinduilwen <ladylaurelin@hotmail.com>
2019-06-16 14:38:53 +0200

    pushing the palindrome

--- app.js
+++ app.js
@@ -39,14 +39,65 @@ newUser.hasAccess = true;
 //One of bool, string, func, array, number
 
 
-const hyfclass19 = {
-    name: "class19",
-    doesExist: true,
-    studentNumber: 15,
-    studentNames: ["name1", "name2", "name3"],
+const hyfClasses = [{
+        name: "class19",
+        doesExist: true,
+        studentNumber: 15,
+        studentNames: ["name1", "name2", "name3"],
 
-    saySomething: function() {
-        console.log("something");
+        saySomething: function() {
+            console.log("something");
+
+        },
+    },
+
+    {
+        name: "class22",
+        doesExist: true,
+        studentNumber: 15,
+        studentNames: ["name1", "name2", "name3"],
+
+        // saySomething: function() {
+        //     console.log("something");          //old model
+        // },
+
+        saySomething: () => {
+            console.log("something");
+        }
     }
+];
 
+console.log(hyfClasses[0].saySomething())
+
+// hyfClasses.forEach(() =>
+
+//write a function
+
+const addTwoNumbers = (a, b) => {
+    return a + b;
 };
+
+const myNumber = addTwoNumbers(5, 2);
+console.log(myNumber);
+
+const myString = "astring";
+
+console.log(myString.charAt(1));
+
+//palindrome
+
+const isPalindrome = (str) => {
+
+    const lenght = str.lenght;
+    let palindromeFlag = true;
+    for (let i = 0; i < lenght; i++) {
+        if (str.charAt(i) !== strcharAt(str.lenght - 1 - i)) {
+            palindromeFlag = false;
+        }
+    }
+    return palindromeFlag;
+};
+
+const result = isPalindrome("tacocat");
+
+console.log(result);
