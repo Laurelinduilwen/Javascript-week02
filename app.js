f2de1c20f3d2298abffc2f310c8519024b2a7b6c
Laurelinduilwen <ladylaurelin@hotmail.com>
2019-06-16 12:53:21 +0200

    something something

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
 console.log("hello");
+
+//obejcts
+//items that has properties
+//Key : Value
+
+const newUser = {
+    userName: "Arya",
+    age: 32,
+    friends: [],
+    hasAccess: false,
+    pet: [{
+            type: 'cat',
+            age: 2
+        },
+        {
+            type: "turtle",
+            age: 100
+        }
+    ], //Array of objects
+
+    sayHello: function() {
+        console.log("hello");
+    }
+
+};
+
+const petProperty = "pet"; //need this for the bracket notation
+
+newUser.sayHello(); //accessing property of an object one method
+
+console.log(newUser[petProperty]); //proper way to call this, bracket notation to access a property of an object
+
+console.log(typeof newUser);
+
+newUser.hasAccess = true;
+
+// Make an object hyfclass19
+//One of bool, string, func, array, number
+
+
+const hyfclass19 = {
+    doesItExist: true,
+    studentNumber: 15,
+    studentNames: ["name1", "name2", "name3"],
+
+    saySomething: function() {
+        console.log("something");
+    }
+
+};
